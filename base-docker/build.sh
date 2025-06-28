3c4185b35d0dcb4ca6be3550a9180d4155a7d849
XLion <xlion@xlion.tw>
2025-06-29 03:18:20 +0800

    base-docker

---
+++ base-docker/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %almalinux)"
+
+dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+systemctl enable docker
