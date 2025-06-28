bbab7d5e20dc7e04420c22bfc31bc22cb6028df6
XLion <xlion@xlion.tw>
2025-06-28 22:31:54 +0800

    stage1

---
+++ stage1/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %almalinux)"
+
+# https://github.com/blue-build/modules/blob/bc0cfd7381680dc8d4c60f551980c517abd7b71f/modules/rpm-ostree/rpm-ostree.sh#L16
+echo "Creating symlinks to fix packages that install to /opt"
+# Create symlink for /opt to /var/opt since it is not created in the image yet
+mkdir -p "/var/opt"
+ln -s "/var/opt"  "/opt"
+
+dnf install -y 'dnf-command(config-manager)'
+
+dnf config-manager --enable crb
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/centos/10/tailscale.repo
+dnf install -y https://dl.fedoraproject.org/pub/epel/10/Everything/x86_64/Packages/e/epel-release-10-6.el10_0.noarch.rpm
+dnf upgrade -y
+
+dnf install -y screen vim htop wget git tailscale systemd-resolved ncdu
+
+systemctl enable tailscaled
+systemctl enable systemd-resolved
+
+# ln -sf ../run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
