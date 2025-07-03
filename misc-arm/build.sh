9d56c15fbb6303d2f68521827b44aaa33fed4c9d
XLion <xlion@xlion.tw>
2025-07-03 19:12:49 +0800

    misc to base

--- misc-arm/build.sh
+++ misc-arm/build.sh
@@ -5,7 +5,4 @@ set -ouex pipefail
 RELEASE="$(rpm -E %almalinux)"
 
 dnf group -y install 'Development Tools'
-ln -sf /usr/bin/dnf /usr/bin/yum
-curl -s https://install.crowdsec.net | sh
 
-dnf -y install crowdsec-firewall-bouncer-nftables
