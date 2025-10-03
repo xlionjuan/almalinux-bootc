9e740a560fd7e9a592e64578e598bb07e4da28fa
XLion <baby20030720@gmail.com>
2025-10-04 06:21:47 +0800

    Docker repo CentOS -> RHEL

--- base-docker/build.sh
+++ base-docker/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %almalinux)"
 
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
