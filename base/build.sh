0c3ba3f46bbe8228a5e68d7c5744927704844398
XLion <xlion@xlion.tw>
2026-08-21 21:02:18 +0800

    turn back alma to rhel

--- base/build.sh
+++ base/build.sh
@@ -276,7 +276,7 @@ EOF
 
 ########## Docker ##########
 
-dnf config-manager --add-repo https://download.docker.com/linux/alma/docker-ce.repo
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
 dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
